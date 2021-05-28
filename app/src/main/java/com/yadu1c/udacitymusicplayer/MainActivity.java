d40b5c2321fe6aed8f4f20abdb5c6e969b48d28c
vikram <Krushit@DESKTOP-CICCLTS>
2021-05-28 10:59:49 -0400

    implementation of listview and adapter for listview done - designing remaing

--- app/src/main/java/com/yadu1c/udacitymusicplayer/MainActivity.java
+++ app/src/main/java/com/yadu1c/udacitymusicplayer/MainActivity.java
@@ -2,7 +2,17 @@ package com.yadu1c.udacitymusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.yadu1c.udacitymusicplayer.Model.Adapter.SongApapter;
+import com.yadu1c.udacitymusicplayer.Model.Song;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Song[] songs = { new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign),
+        new Song("Beedi (from Omkara)", "Sunidhi Chauhan", 00.00, 04.53, R.drawable.finaldesign)
+        };
+        SongApapter songsadapter=new SongApapter(this, Arrays.asList(songs));
+        ListView songlistview=findViewById(R.id.songlistview);
+        songlistview.setAdapter(songsadapter);
+
     }
 }
---
+++ app/src/main/java/com/yadu1c/udacitymusicplayer/Model/Song.java
@@ -0,0 +1,36 @@
+package com.yadu1c.udacitymusicplayer.Model;
+
+public class Song {
+String mName;
+String mSinger;
+int mThumbnail;
+double mStartTime;
+double mEndTime;
+public Song(String sName,String sSinger,double sStartTime,double sEndTime,int sThumbnail){
+    mStartTime=sStartTime;
+    mEndTime=sEndTime;
+    mSinger=sSinger;
+    mName=sName;
+    mThumbnail=sThumbnail;
+}
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmSinger() {
+        return mSinger;
+    }
+
+    public int getmThumbnail() {
+        return mThumbnail;
+    }
+
+    public double getmStartTime() {
+        return mStartTime;
+    }
+
+    public double getmEndTime() {
+        return mEndTime;
+    }
+}
