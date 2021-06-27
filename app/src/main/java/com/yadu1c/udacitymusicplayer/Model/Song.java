bcabb69454135da46acd6180b764d7bc13a1cebf
vikram <Krushit@DESKTOP-CICCLTS>
2021-06-27 11:51:21 -0400

    removed blank lines and reformatted code

--- app/src/main/java/com/yadu1c/udacitymusicplayer/Model/Song.java
+++ app/src/main/java/com/yadu1c/udacitymusicplayer/Model/Song.java
@@ -1,18 +1,19 @@
 package com.yadu1c.udacitymusicplayer.Model;
 
 public class Song {
-String mName;
-String mSinger;
-int mThumbnail;
-double mStartTime;
-double mEndTime;
-public Song(String sName,String sSinger,double sStartTime,double sEndTime,int sThumbnail){
-    mStartTime=sStartTime;
-    mEndTime=sEndTime;
-    mSinger=sSinger;
-    mName=sName;
-    mThumbnail=sThumbnail;
-}
+    String mName;
+    String mSinger;
+    int mThumbnail;
+    double mStartTime;
+    double mEndTime;
+
+    public Song(String sName, String sSinger, double sStartTime, double sEndTime, int sThumbnail) {
+        mStartTime = sStartTime;
+        mEndTime = sEndTime;
+        mSinger = sSinger;
+        mName = sName;
+        mThumbnail = sThumbnail;
+    }
 
     public String getmName() {
         return mName;
