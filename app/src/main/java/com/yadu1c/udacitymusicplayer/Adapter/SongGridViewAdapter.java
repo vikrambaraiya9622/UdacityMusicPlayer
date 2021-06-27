7815ee460d273ec64f203a88f72bd2940d5d1241
vikram <Krushit@DESKTOP-CICCLTS>
2021-06-27 12:04:13 -0400

    Removed blank lines and reformatted SongGridViewAdapter

--- app/src/main/java/com/yadu1c/udacitymusicplayer/Adapter/SongGridViewAdapter.java
+++ app/src/main/java/com/yadu1c/udacitymusicplayer/Adapter/SongGridViewAdapter.java
@@ -7,13 +7,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import com.yadu1c.udacitymusicplayer.Model.Song;
 import com.yadu1c.udacitymusicplayer.R;
-
 import java.util.List;
 
 public class SongGridViewAdapter extends ArrayAdapter<Song> {
@@ -24,17 +21,16 @@ public class SongGridViewAdapter extends ArrayAdapter<Song> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        if (convertView==null){
-            convertView= LayoutInflater.from(parent.getContext()).inflate(R.layout.songgrid,parent,false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.songgrid, parent, false);
         }
-        Song currentSong=getItem(position);
-        ImageView mthumbnail=convertView.findViewById(R.id.thumbnail);
-        TextView msongname=convertView.findViewById(R.id.songName);
-        TextView msingername=convertView.findViewById(R.id.singerName);
-
+        Song currentSong = getItem(position);
+        ImageView mthumbnail = convertView.findViewById(R.id.thumbnail);
+        TextView msongname = convertView.findViewById(R.id.songName);
+        TextView msingername = convertView.findViewById(R.id.singerName);
         mthumbnail.setImageResource(currentSong.getmThumbnail());
         msongname.setText(currentSong.getmName());
         msingername.setText(currentSong.getmSinger());
-        return  convertView;
+        return convertView;
     }
 }
